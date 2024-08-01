634265cdd932d3479a982c20ed051152c212f88a
Yusuf Abdallah <62337264+yuzalsif@users.noreply.github.com>
2024-08-01 11:45:26 +0300

    Use new link url for api endpoint

--- docs/main.js
+++ docs/main.js
@@ -9,7 +9,7 @@ const modalGrowth = document.getElementById('modalGrowth');
 const modalClose = document.getElementsByClassName('close')[0];
 
 // Fetch cities
-fetch('https://tinyurl.com/5bzzvh6u')
+fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
     .then(response => response.json())
     .then(data => {
         // Function to filter cities based on search query
